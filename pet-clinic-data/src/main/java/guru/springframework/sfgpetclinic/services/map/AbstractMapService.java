0c47e48b3d70bbac0b87f83e712bce6367d3dabc
MarcoBruno97 <marco.bruno@libero.it>
2022-01-21 11:34:12 +0100

    Refactoring Map impl to abstract ID generation. closes #23

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,14 +1,12 @@
 package guru.springframework.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import guru.springframework.sfgpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T,ID> {
+import java.util.*;
 
-    protected Map<ID,T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity,ID extends Long> {//extends Long lo puoi togliere, dato che Long e' final
 
+    protected Map<Long,T> map = new HashMap<>();
     Set<T> findAll(){
         return new HashSet<>(map.values());
     }
@@ -17,8 +15,19 @@ public abstract class AbstractMapService<T,ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id, object);
+    T save(T object){
+
+        // guarding clause
+        if (object == null) {
+            throw new RuntimeException("Object cannot be null");
+        }
+
+        // update id
+        if (object.getId() == null) {
+            object.setId(getNextId());
+        }
+        map.put(object.getId(), object);
+
         return object;
     }
 
@@ -30,4 +39,7 @@ public abstract class AbstractMapService<T,ID> {
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
     }
 
+    private Long getNextId(){
+        return map.isEmpty() ? 1L : Collections.max(map.keySet()) + 1L;
+    }
 }
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -22,7 +22,7 @@ public class DataLoader implements CommandLineRunner {//CommandLineRunner is a S
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
+        //owner1.setId(1L);
         owner1.setFirstName("Micheal");
         owner1.setLastName("Weston");
 
@@ -31,7 +31,7 @@ public class DataLoader implements CommandLineRunner {//CommandLineRunner is a S
 
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
+        //owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
 
@@ -40,7 +40,7 @@ public class DataLoader implements CommandLineRunner {//CommandLineRunner is a S
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
+        //vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
