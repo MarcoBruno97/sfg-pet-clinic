5391295671de9162ac24409aed65dd71cd2254e7
MarcoBruno97 <marco.bruno@libero.it>
2022-01-25 19:09:07 +0100

    added vets data in bootstrap. closes #48

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -5,9 +5,11 @@ import guru.springframework.sfgpetclinic.services.CrudService;
 import java.util.Set;
 import  guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile({"default", "map"})
 public class PetServiceMap extends AbstractMapService<Pet,Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
