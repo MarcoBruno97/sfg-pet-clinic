ee8dddfe82e5b7246caf57a219923e3fdf64f1fa
MarcoBruno97 <marco.bruno@libero.it>
2022-01-25 15:16:42 +0100

    vet jpa. closes #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,10 +1,13 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
 public class Vet extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER)//this means that when vet data will be fetched, will also be fetched specialties data from db
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_ud"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();// it's for NullPointerException
 
     public Set<Speciality> getSpecialities() {
