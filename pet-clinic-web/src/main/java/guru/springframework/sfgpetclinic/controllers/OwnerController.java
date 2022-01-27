0d6114f8dfaa868f9c6ca939418cccbb4351947a
MarcoBruno97 <marco.bruno@libero.it>
2022-01-27 12:56:26 +0100

    Added MockMvc tests for owner controller.closes #52

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")//thi is a prefixed for others paths
 @Controller
+
 public class OwnerController {
 
     private final OwnerService ownerService;
